98f03af148eada571f61a70e664e16cbdc53572b
Wei Shen <shenwei356@gmail.com>
2018-06-08 23:26:31 +0800

    fix typo

--- js/base.js
+++ js/base.js
@@ -1,5 +1,3 @@
-/* Search */
-
 function getSearchTerm()
 {
     var sPageURL = window.location.search.substring(1);
@@ -7,30 +5,74 @@ function getSearchTerm()
     for (var i = 0; i < sURLVariables.length; i++)
     {
         var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == 'q') {
+        if (sParameterName[0] == 'q')
+        {
             return sParameterName[1];
         }
     }
 }
 
 $(document).ready(function() {
-    var search_term = getSearchTerm(),
-        $search_modal = $('#mkdocs_search_modal');
 
-    if(search_term) {
+    var search_term = getSearchTerm(),
+        $search_modal = $('#mkdocs_search_modal'),
+        $keyboard_modal = $('#mkdocs_keyboard_modal');
+
+    if(search_term){
         $search_modal.modal();
     }
 
+    // make sure search input gets autofocus everytime modal opens.
     $search_modal.on('shown.bs.modal', function () {
         $search_modal.find('#mkdocs-search-query').focus();
     });
-});
 
+    // Keyboard navigation
+    document.addEventListener("keydown", function(e) {
+        if ($(e.target).is(':input')) return true;
+        var key = e.which || e.keyCode || window.event && window.event.keyCode;
+        var page;
+        switch (key) {
+            case 39:   // right arrow
+                page = $('[role="navigation"] a:contains(Next):first').prop('href');
+                break;
+            case 37:   // left arrow
+                page = $('[role="navigation"] a:contains(Previous):first').prop('href');
+                break;
+            case 83:   // s
+                e.preventDefault();
+                $keyboard_modal.modal('hide');
+                $search_modal.modal('show');
+                $search_modal.find('#mkdocs-search-query').focus();
+                break;
+            case 191:  // ?
+                $keyboard_modal.modal('show');
+                break;
+            default: break;
+        }
+        if (page) {
+            $keyboard_modal.modal('hide');
+            window.location.href = page;
+        }
+    });
 
-/* Highlight */
-$( document ).ready(function() {
+    // Highlight.js
     hljs.initHighlightingOnLoad();
     $('table').addClass('table table-striped table-hover');
+
+    // Improve the scrollspy behaviour when users click on a TOC item.
+    $(".bs-sidenav a").on("click", function() {
+        var clicked = this;
+        setTimeout(function() {
+            var active = $('.nav li.active a');
+            active = active[active.length - 1];
+            if (clicked !== active) {
+                $(active).parent().removeClass("active");
+                $(clicked).parent().addClass("active");
+            }
+        }, 50);
+    });
+
 });
 
 
@@ -48,6 +90,3 @@ $('a').click(function(e) {
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
-
-
-
