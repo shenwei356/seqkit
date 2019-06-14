ce4f82ac5b0406d513d704043e8677e527b7e98b
Botond Sipos <Botond.Sipos@nanoporetech.com>
2019-06-14 08:10:31 +0100

    Added fish subcommand - look for short sequence tags using local alignment.

---
+++ seqkit/cmd/fish.go
@@ -0,0 +1,424 @@
+// Copyright © 2016 Wei Shen <shenwei356@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/biogo/hts/bam"
+	"github.com/biogo/hts/sam"
+	"github.com/shenwei356/bio/seq"
+	"github.com/shenwei356/bio/seqio/fastx"
+	"github.com/shenwei356/util/byteutil"
+	"github.com/shenwei356/xopen"
+	"github.com/spf13/cobra"
+)
+
+func parseRanges(rf string) Ranges {
+	res := Ranges{}
+	if len(rf) == 0 {
+		return Ranges{Range{math.NaN(), math.NaN()}}
+	}
+	rst := strings.Split(strings.TrimSpace(rf), ",")
+	for _, t := range rst {
+		t = strings.TrimSpace(t)
+		if len(t) == 0 {
+			continue
+		}
+		rt := strings.Split(t, ":")
+		if len(rt) != 2 {
+			panic("invalid range: " + t)
+		}
+		var err error
+		var ts, te string
+		var s, e float64
+		ts = strings.TrimSpace(rt[0])
+		te = strings.TrimSpace(rt[1])
+		if len(ts) == 0 {
+			s = math.NaN()
+		} else {
+			s, err = strconv.ParseFloat(ts, 64)
+			checkError(err)
+		}
+		if len(te) == 0 {
+			e = math.NaN()
+		} else {
+			e, err = strconv.ParseFloat(te, 64)
+			checkError(err)
+		}
+		if e < s {
+			panic("invalid range: " + t)
+		}
+		res = append(res, Range{s, e})
+	}
+	return res
+}
+
+// fishCmd represents the seq command
+var fishCmd = &cobra.Command{
+	Use:   "fish",
+	Short: "look for short sequences in larger sequences using local alignment",
+	Long:  "look for short sequences in larger sequences using local alignment",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		config := getConfigs(cmd)
+		alphabet := config.Alphabet
+		idRegexp := config.IDRegexp
+		lineWidth := config.LineWidth
+		outFile := config.OutFile
+
+		flagMinQual := getFlagFloat64(cmd, "min-qual")
+		flagCutoff := 1 - math.Pow(10, flagMinQual/-10)
+		_ = flagCutoff
+
+		seq.AlphabetGuessSeqLengthThreshold = config.AlphabetGuessSeqLength
+		runtime.GOMAXPROCS(config.Threads)
+
+		flagPass := getFlagBool(cmd, "pass")
+		flagAln := getFlagBool(cmd, "print-aln")
+		queryFastx := getFlagString(cmd, "query-fastx")
+		flagSeq := getFlagString(cmd, "query-sequences")
+		flagBam := getFlagString(cmd, "out-bam")
+		flagRange := getFlagString(cmd, "ranges")
+		flagNullMode := getFlagString(cmd, "null-mode")
+		flagStranded := getFlagBool(cmd, "stranded")
+		flagAll := getFlagBool(cmd, "all")
+		flagDesc := getFlagBool(cmd, "print-desc")
+		flagInvert := getFlagBool(cmd, "invert")
+
+		ranges := parseRanges(flagRange)
+
+		validateSeq := getFlagBool(cmd, "validate-seq")
+		validateSeqLength := getFlagValidateSeqLength(cmd, "validate-seq-length")
+
+		seq.ValidateSeq = validateSeq
+		seq.ValidateWholeSeq = false
+		seq.ValidSeqLengthThreshold = validateSeqLength
+		seq.ValidSeqThreads = config.Threads
+
+		if !(alphabet == nil || alphabet == seq.Unlimit) {
+			seq.ValidateSeq = true
+		}
+
+		files := getFileList(args)
+		var alns []*AlignedSeq
+		if len(files) == 0 {
+			files = []string{"-"}
+		}
+
+		detector := NewSeqDetector(flagAll, flagStranded, flagNullMode, flagCutoff)
+		if queryFastx != "" {
+			detector.LoadQueries(queryFastx)
+		}
+		if flagSeq != "" {
+			detector.AddAnonQueries(strings.Split(flagSeq, ","))
+		}
+
+		outfh, err := xopen.Wopen(outFile)
+		checkError(err)
+
+		var checkSeqType bool
+		var isFastq bool
+		var printQual bool
+		var head []byte
+		var text []byte
+		var b *bytes.Buffer
+		var record *fastx.Record
+		var sequence *seq.Seq
+		var fastxReader *fastx.Reader
+		var count int
+		var refMap map[string]int
+		var samRefs []*sam.Reference
+		if flagBam != "" {
+			alns = make([]*AlignedSeq, 0, 1024)
+			samRefs = make([]*sam.Reference, 0, 1024)
+			refMap = make(map[string]int, 1024)
+		}
+		first := true
+
+		for _, file := range files {
+			fastxReader, err = fastx.NewReader(alphabet, file, idRegexp)
+			checkError(err)
+
+			checkSeqType = true
+			printQual = false
+			for {
+				record, err = fastxReader.Read()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					checkError(err)
+					break
+				}
+
+				if checkSeqType {
+					isFastq = fastxReader.IsFastq
+					if isFastq {
+						config.LineWidth = 0
+						printQual = true
+					}
+					checkSeqType = false
+				}
+
+				refId := string(record.Name)
+				if !flagDesc {
+					refId = strings.Split(refId, " ")[0]
+				}
+
+				hits := detector.Detect(&Reference{refId, string(record.Seq.Seq), ranges})
+
+				if !flagInvert {
+					for _, h := range hits {
+						if first {
+							fmt.Fprintf(os.Stderr, "%s\n", strings.Join(h.Fields(), "\t"))
+						}
+						first = false
+						fmt.Fprintf(os.Stderr, "%s\n", h)
+						if flagAln {
+							fmt.Fprintf(os.Stderr, "%s\n", h.AlnString())
+						}
+						h.Ref.Seq = ""
+					}
+
+					if flagBam != "" {
+						nr, _ := sam.NewReference(refId, "", "", len(record.Seq.Seq), nil, nil)
+						samRefs = append(samRefs, nr)
+						refMap[refId] = count
+						alns = append(alns, hits...)
+					}
+				} else {
+					if first {
+						fmt.Fprintf(os.Stderr, "Ref\n")
+						first = false
+					}
+					if len(hits) == 0 {
+						fmt.Fprintf(os.Stderr, "%s\n", refId)
+					}
+				}
+
+				count++
+				if !flagPass {
+					continue
+				}
+
+				head = record.Name
+				sequence = record.Seq
+
+				if isFastq {
+					outfh.WriteString("@")
+					outfh.Write(head)
+					outfh.WriteString("\n")
+				} else {
+					outfh.WriteString(">")
+					outfh.Write(head)
+					outfh.WriteString("\n")
+				}
+
+				if len(sequence.Seq) <= pageSize {
+					outfh.Write(byteutil.WrapByteSlice(sequence.Seq, config.LineWidth))
+				} else {
+					if bufferedByteSliceWrapper == nil {
+						bufferedByteSliceWrapper = byteutil.NewBufferedByteSliceWrapper2(1, len(sequence.Seq), config.LineWidth)
+					}
+					text, b = bufferedByteSliceWrapper.Wrap(sequence.Seq, config.LineWidth)
+					outfh.Write(text)
+					outfh.Flush()
+					bufferedByteSliceWrapper.Recycle(b)
+				}
+
+				outfh.WriteString("\n")
+
+				if printQual {
+					outfh.WriteString("+\n")
+
+					if len(sequence.Qual) <= pageSize {
+						outfh.Write(byteutil.WrapByteSlice(sequence.Qual, config.LineWidth))
+					} else {
+						if bufferedByteSliceWrapper == nil {
+							bufferedByteSliceWrapper = byteutil.NewBufferedByteSliceWrapper2(1, len(sequence.Qual), config.LineWidth)
+						}
+						text, b = bufferedByteSliceWrapper.Wrap(sequence.Qual, config.LineWidth)
+						outfh.Write(text)
+						outfh.Flush()
+						bufferedByteSliceWrapper.Recycle(b)
+					}
+
+					outfh.WriteString("\n")
+				}
+
+			} // record
+			config.LineWidth = lineWidth
+
+		} //file
+
+		if flagBam != "" {
+			saveBam(flagBam, samRefs, refMap, alns)
+		}
+		outfh.Close()
+	},
+}
+
+func saveBam(bamFile string, refs []*sam.Reference, refMap map[string]int, alns []*AlignedSeq) {
+	var err error
+	var bamWriter *bam.Writer
+	//fh, err := xopen.Wopen(bamFile)
+	fh, err := os.Create(bamFile)
+	checkError(err)
+
+	checkError(err)
+	var h *sam.Header
+	h, err = sam.NewHeader([]byte{}, refs)
+	checkError(err)
+	fish := sam.NewProgram("seqkit", "seqkit", "seqkit fish", "-", "1.0")
+	h.AddProgram(fish)
+	bamWriter, err = bam.NewWriter(fh, h, 50)
+	checkError(err)
+	for _, a := range alns {
+		mq := -10 * math.Log10(1.0-(a.Score/a.Query.NullScore))
+		if math.IsNaN(mq) || mq > 60 {
+			mq = 60
+		}
+		var record *sam.Record
+		pg, err := sam.NewAux(sam.NewTag("PG"), 0)
+		record, err = NewSAMRecordFromAln(a.Query.Name, refs[refMap[a.Ref.Name]], a.RefStart, a.RefEnd, a.QueryStart, a.QueryEnd, a.RefAln, a.QueryAln, a.Query.Strand, byte(uint8(mq)), a.Query.Seq, nil, []sam.Aux{pg})
+		checkError(err)
+		if !sam.IsValidRecord(record) {
+			panic("failed to build BAM record from raw alignment: \n" + a.String() + "\n" + record.String())
+		}
+		if !a.Best {
+			record.Flags |= sam.Secondary
+		}
+
+		err = bamWriter.Write(record)
+		checkError(err)
+	}
+	checkError(bamWriter.Close())
+	checkError(fh.Close())
+}
+
+func init() {
+	RootCmd.AddCommand(fishCmd)
+
+	fishCmd.Flags().BoolP("validate-seq", "v", false, "validate bases according to the alphabet")
+	fishCmd.Flags().BoolP("pass", "x", false, "pass through mode (write input to stdout)")
+	fishCmd.Flags().BoolP("all", "a", false, "search all")
+	fishCmd.Flags().StringP("query-fastx", "f", "", "query fasta")
+	fishCmd.Flags().StringP("query-sequences", "F", "", "query sequences")
+	fishCmd.Flags().StringP("out-bam", "b", "", "save aligmnets to this BAM file (memory intensive)")
+	fishCmd.Flags().BoolP("stranded", "s", false, "search + strand only")
+	fishCmd.Flags().StringP("ranges", "r", "", "target ranges, for example: \":10,30:40,-20\"")
+	fishCmd.Flags().StringP("null-mode", "z", "self", "null mode")
+	fishCmd.Flags().BoolP("print-desc", "D", false, "print full sequence header")
+	fishCmd.Flags().BoolP("print-aln", "g", false, "print sequence alignments")
+	fishCmd.Flags().BoolP("invert", "i", false, "print out references not matching with any query")
+	fishCmd.Flags().IntP("validate-seq-length", "V", 10000, "length of sequence to validate (0 for whole seq)")
+	fishCmd.Flags().Float64P("min-qual", "q", 5.0, "minimum mapping quality")
+}
+
+// NewRecordFromAln build a new SAM record based on the provided local alignment and its reference/query coordinates.
+func NewSAMRecordFromAln(name string, ref *sam.Reference, refStart, refEnd, queryStart, queryEnd int, refAln, queryAln string, strand string, mapQ byte, seq string, qual []byte, aux []sam.Aux) (*sam.Record, error) {
+	if len(refAln) != len(queryAln) {
+		panic("alignment length mismatch")
+	}
+	if len(refAln) == 0 {
+		panic("empty alignment")
+	}
+	gap := byte('-')
+
+	rawCo := make([]sam.CigarOp, 0, len(seq))
+	co := make([]sam.CigarOp, 0, len(seq))
+	var consumed int
+	var nm int
+
+	// Building the CIGAR in two steps for clarity.
+	if queryStart > 0 {
+		rawCo = append(rawCo, sam.NewCigarOp(sam.CigarSoftClipped, queryStart))
+	}
+	for i := range refAln {
+		if queryAln[i] == gap {
+			rawCo = append(rawCo, sam.NewCigarOp(sam.CigarDeletion, 1))
+			continue
+
+		} else if refAln[i] == gap {
+			rawCo = append(rawCo, sam.NewCigarOp(sam.CigarInsertion, 1))
+			consumed++
+			continue
+		} else {
+			rawCo = append(rawCo, sam.NewCigarOp(sam.CigarMatch, 1))
+			consumed++
+			if queryAln[i] != refAln[i] {
+				nm++
+			}
+			continue
+
+		}
+	} // refAln
+
+	leftover := len(seq) - queryStart - consumed
+	if leftover > 0 {
+		rawCo = append(rawCo, sam.NewCigarOp(sam.CigarSoftClipped, leftover))
+	}
+
+	cop := rawCo[0].Type()
+	length := rawCo[0].Len()
+	var o sam.CigarOp
+	for i := 1; i < len(rawCo); i++ {
+		o = rawCo[i]
+		if o.Type() == cop {
+			length++
+			continue
+		}
+		co = append(co, sam.NewCigarOp(cop, length))
+		length = o.Len()
+		cop = o.Type()
+	}
+	co = append(co, sam.NewCigarOp(o.Type(), length))
+
+	switch strand {
+	case "-":
+		for i, j := 0, len(co)-1; i < j; i, j = i+1, j-1 {
+			co[i], co[j] = co[j], co[i]
+		}
+	case "+":
+	default:
+		panic("Invalid strand: " + strand)
+
+	}
+
+	nmTag, _ := sam.NewAux(sam.NewTag("NM"), nm)
+	aux = append(aux, nmTag)
+	res, err := sam.NewRecord(name, ref, nil, refStart, -1, 0, mapQ, co, []byte(seq), qual, aux)
+	if err != nil {
+		panic(err)
+	}
+	if strand == "-" {
+		res.Flags |= sam.Reverse
+	}
+	return res, err
+}
---
+++ seqkit/cmd/seq_detect.go
@@ -0,0 +1,332 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"math"
+	//"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/biogo/biogo/align"
+	"github.com/biogo/biogo/alphabet"
+	"github.com/biogo/biogo/feat"
+	"github.com/biogo/biogo/seq/linear"
+	"github.com/shenwei356/bio/seqio/fastx"
+)
+
+type Query struct {
+	Name      string
+	Seq       string
+	Strand    string
+	NullScore float64
+}
+
+type Range struct {
+	Start float64
+	End   float64
+}
+
+type Ranges []Range
+
+type Reference struct {
+	Name   string
+	Seq    string
+	Ranges Ranges
+}
+
+type Queries []*Query
+
+type SeqDetector struct {
+	Queries   Queries
+	SearchAll bool
+	Stranded  bool
+	NullMode  string
+	Cutoff    float64
+}
+
+func NewSeqDetector(searchAll bool, stranded bool, nullMode string, cutoff float64) *SeqDetector {
+	return &SeqDetector{Queries{}, searchAll, stranded, nullMode, cutoff}
+}
+
+func (d *SeqDetector) Detect(r *Reference) []*AlignedSeq {
+	var h []*AlignedSeq
+	for _, rr := range r.Ranges {
+		h = append(h, d.detectOnce(r, rr)...)
+	}
+	return h
+}
+
+func actualRange(rr Range, l int) Range {
+	s, e := rr.Start, rr.End
+	if math.IsNaN(rr.Start) {
+		s = 0
+	} else if rr.Start < 0 {
+		s = float64(l) + rr.Start
+	}
+	if s < 0 {
+		s = 0
+	}
+	if math.IsNaN(rr.End) {
+		e = float64(l)
+	} else if rr.End < 0 {
+		e = float64(l) + rr.End
+	}
+	if e > float64(l) {
+		e = float64(l)
+	}
+	if s > e {
+		s, e = 0, float64(l)
+	}
+
+	return Range{s, e}
+}
+
+func (d *SeqDetector) detectOnce(r *Reference, rr Range) []*AlignedSeq {
+	var hits []*AlignedSeq
+	for _, q := range d.Queries {
+		nr := &Reference{r.Name, r.Seq, Ranges{actualRange(rr, len(r.Seq))}}
+		h := PairwiseAlignSW(nr, q)
+		h.Detector = d
+		if (h.Score / q.NullScore) > d.Cutoff {
+			hits = append(hits, h)
+		}
+	}
+	return bestHits(hits, -1)
+}
+
+type byScore []*AlignedSeq
+
+func (a byScore) Len() int           { return len(a) }
+func (a byScore) Less(i, j int) bool { return a[i].Score > a[j].Score }
+func (a byScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func bestHits(h []*AlignedSeq, n int) []*AlignedSeq {
+	if len(h) == 0 {
+		return h
+	}
+	if n < 0 {
+		n = len(h)
+	}
+	res := make([]*AlignedSeq, len(h))
+	copy(res, h)
+	sort.Stable(byScore(res))
+	i := n
+	if i > len(res) {
+		i = len(res)
+	}
+	res[0].Best = true
+	return res[:i]
+}
+
+func (d *SeqDetector) LoadQueries(fx string) {
+	var record *fastx.Record
+	var fastxReader *fastx.Reader
+	var err error
+	if len(d.Queries) == 0 {
+		d.Queries = make(Queries, 0, 10)
+	}
+
+	fastxReader, err = fastx.NewReader(nil, fx, "")
+	checkError(err)
+
+	for {
+		record, err = fastxReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			checkError(err)
+			break
+		}
+
+		seq := string(record.Seq.Seq)
+		name := strings.Split(string(record.Name), " ")[0]
+		ns := d.nullScore(seq)
+		d.Queries = append(d.Queries, &Query{name, seq, "+", ns})
+		if !d.Stranded {
+			d.Queries = append(d.Queries, &Query{name, RevCompDNA(seq), "-", ns})
+		}
+
+	}
+}
+
+func (d *SeqDetector) AddAnonQueries(qrs []string) {
+	for i, q := range qrs {
+		name := fmt.Sprintf("q%d", i)
+		ns := d.nullScore(q)
+		d.Queries = append(d.Queries, &Query{name, q, "+", ns})
+		if !d.Stranded {
+			d.Queries = append(d.Queries, &Query{name, RevCompDNA(q), "-", ns})
+		}
+	}
+
+}
+
+func (d *SeqDetector) nullScore(q string) float64 {
+	switch d.NullMode {
+	case "self":
+		return PairwiseAlignSW(&Reference{Name: "Ref", Seq: q, Ranges: Ranges{Range{0, float64(len(q))}}}, &Query{Name: "Query", Seq: q}).Score
+	}
+	return math.NaN()
+}
+
+//Interface for getting alignment score.
+type Scorer interface {
+	Score() int
+}
+
+// Make a new anonymous linear.Seq.
+func NewAnonLinearSeq(s string) *linear.Seq {
+	return &linear.Seq{Seq: alphabet.BytesToLetters([]byte(s))}
+}
+
+//Pairwise alignment of two sequences by biogo.
+func PairwiseAlignSW(r *Reference, q *Query) *AlignedSeq {
+	ref := NewAnonLinearSeq(r.Seq[int(r.Ranges[0].Start):int(r.Ranges[0].End)])
+	ref.Alpha = alphabet.DNAgapped
+	query := NewAnonLinearSeq(q.Seq)
+	query.Alpha = alphabet.DNAgapped
+
+	smith := align.SWAffine{
+		Matrix: align.Linear{
+			{0, -1, -1, -1, -1},
+			{-1, 1, -1, -1, -1},
+			{-1, -1, 1, -1, -1},
+			{-1, -1, -1, 1, -1},
+			{-1, -1, -1, -1, 1},
+		},
+		GapOpen: -1,
+	}
+
+	aln, err := smith.Align(ref, query)
+	var res *AlignedSeq
+	if err == nil {
+		fa := align.Format(ref, query, aln, '-')
+		res = AlignInfo(r, q, aln)
+		res.RefAln = fmt.Sprintf("%s", fa[0])
+		res.QueryAln = fmt.Sprintf("%s", fa[1])
+	} else {
+		panic("Could not align sequences!")
+	}
+	return res
+}
+
+type AlignedSeq struct {
+	Ref        *Reference
+	Query      *Query
+	QueryAln   string
+	RefAln     string
+	RefStart   int
+	RefEnd     int
+	QueryStart int
+	QueryEnd   int
+	Score      float64
+	Best       bool
+	Detector   *SeqDetector
+}
+
+func (a *AlignedSeq) Fields() []string {
+	validFields := []string{"Ref", "RefStart", "RefEnd", "Query", "QueryStart", "QueryEnd", "Strand", "MapQual", "RawScore", "Acc", "ClipAcc", "QueryCov"}
+	return validFields
+}
+func (a *AlignedSeq) String() string {
+	validFields := []string{"Ref", "RefStart", "RefEnd", "Query", "QueryStart", "QueryEnd", "Strand", "MapQual", "RawScore", "Acc", "ClipAcc", "QueryCov"}
+	fmap := make(map[string]func(*AlignedSeq) string)
+	fmap["Ref"] = func(a *AlignedSeq) string {
+		return a.Ref.Name
+	}
+	fmap["RefStart"] = func(a *AlignedSeq) string {
+		return strconv.Itoa(a.RefStart)
+	}
+	fmap["RefEnd"] = func(a *AlignedSeq) string {
+		return strconv.Itoa(a.RefEnd)
+	}
+	fmap["Query"] = func(a *AlignedSeq) string {
+		return a.Query.Name
+	}
+	fmap["QueryStart"] = func(a *AlignedSeq) string {
+		return strconv.Itoa(a.QueryStart)
+	}
+	fmap["QueryEnd"] = func(a *AlignedSeq) string {
+		return strconv.Itoa(a.QueryEnd)
+	}
+	fmap["Strand"] = func(a *AlignedSeq) string {
+		return a.Query.Strand
+	}
+	fmap["RawScore"] = func(a *AlignedSeq) string {
+		return fmt.Sprintf("%.0f", a.Score)
+	}
+	fmap["MapQual"] = func(a *AlignedSeq) string {
+		mq := -10 * math.Log10(1-a.Score/a.Query.NullScore)
+		if math.IsInf(mq, 1) {
+			mq = 60
+		}
+		return fmt.Sprintf("%.0f", mq)
+	}
+	fmap["Acc"] = func(a *AlignedSeq) string {
+		diff := 0
+		for i, rb := range []byte(a.RefAln) {
+			if rb != a.QueryAln[i] {
+				diff++
+			}
+		}
+		length := float64(len(a.RefAln))
+		acc := (length - float64(diff)) * 100 / length
+		return fmt.Sprintf("%.2f", acc)
+	}
+	fmap["ClipAcc"] = func(a *AlignedSeq) string {
+		diff := 0
+		for i, rb := range []byte(a.RefAln) {
+			if rb != a.QueryAln[i] {
+				diff++
+			}
+		}
+		length := float64(len(a.RefAln))
+		acc := ((length - float64(diff)) * 100) / (length + float64(len(a.Query.Seq)-a.QueryEnd+a.QueryStart))
+		return fmt.Sprintf("%.2f", acc)
+	}
+	fmap["QueryCov"] = func(a *AlignedSeq) string {
+		acc := float64(a.QueryEnd-a.QueryStart) * 100 / float64(len(a.Query.Seq))
+		return fmt.Sprintf("%.2f", acc)
+	}
+
+	tmp := make([]string, len(validFields))
+	for i, f := range validFields {
+		tmp[i] = fmap[f](a)
+	}
+
+	return strings.Join(tmp, "\t")
+}
+
+func (a *AlignedSeq) AlnString() string {
+	return fmt.Sprintf("@\t%s\t+\t%d\t%d\t%s\n@\t%s\t%s\t%d\t%d\t%s", a.RefAln, a.RefStart, a.RefEnd, a.Ref.Name, a.QueryAln, a.Query.Strand, a.QueryStart, a.QueryEnd, a.Query.Name)
+}
+
+//Construct AlignedSeq structure based on raw alignment results.
+func AlignInfo(r *Reference, q *Query, f []feat.Pair) *AlignedSeq {
+	ref_starts := make([]int, 0)
+	ref_ends := make([]int, 0)
+	query_starts := make([]int, 0)
+	query_ends := make([]int, 0)
+	scores := make([]int, 0)
+
+	for _, fs := range f {
+		fc := fs.Features()
+		fsScorer, _ := fs.(Scorer)
+		scores = append(scores, fsScorer.Score())
+		ref_starts = append(ref_starts, fc[0].Start())
+		ref_ends = append(ref_ends, fc[0].End())
+		query_starts = append(query_starts, fc[1].Start())
+		query_ends = append(query_ends, fc[1].End())
+
+	}
+	res := &AlignedSeq{Ref: r, Query: q}
+	res.RefStart = MinInts(ref_starts)
+	res.RefEnd = MaxInts(ref_ends)
+	res.QueryStart = MinInts(query_starts)
+	res.QueryEnd = MaxInts(query_ends)
+	res.Score = float64(SumInts(scores))
+	return res
+}
