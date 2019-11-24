5a8be9b2e603877a007ac9897ed8acd8334e2d2f
Botond Sipos <sbotond@gmail.com>
2019-11-24 15:55:27 +0000

    +

--- seqkit/cmd/sana.go
+++ seqkit/cmd/sana.go
@@ -21,6 +21,8 @@
 package cmd
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -30,6 +32,15 @@ import (
 
 	"github.com/shenwei356/xopen"
 	"github.com/spf13/cobra"
+)
+
+type SeqStreamCtrl int
+
+const (
+	StreamTry SeqStreamCtrl = iota
+	StreamQuit
+	StreamEOF
+	StreamExited
 )
 
 // sanaCmd represents the sana command
@@ -52,15 +63,14 @@ var sanaCmd = &cobra.Command{
 		files := getFileList(args)
 
 		for _, file := range files {
-			rawSeqChan := NewRawSeqStream(file, 1000, qBase)
+			rawSeqChan := NewRawSeqStreamFromFile(file, 1000, qBase, "fasta")
 
-			prevRec := "None"
 			for rawSeq := range rawSeqChan {
-				if err := ValidateSeq(rawSeq); err != nil {
-					fmt.Fprintf(os.Stderr, "Discarded record\t%s\t: %s\tPrevious record:%s\n", rawSeq.Id, err, prevRec)
-				} else {
-					prevRec = rawSeq.Id
+				switch rawSeq.Err {
+				case nil:
 					outfh.WriteString(rawSeq.String() + "\n")
+				default:
+					os.Stderr.WriteString(rawSeq.String() + "\n")
 				}
 			}
 		}
@@ -75,14 +85,22 @@ func init() {
 
 // simpleSeq is a structure holding basic sequnce information with qualities.
 type simpleSeq struct {
-	Id    string
-	Seq   string
-	Qual  []int
-	QBase int
+	Id        string
+	Seq       string
+	Qual      []int
+	QBase     int
+	Err       error
+	StartLine int
 }
 
 // String generates a string representation of a pointer to simpleSeq.
 func (s *simpleSeq) String() string {
+	if s.Err != nil {
+		return fmt.Sprintf("%s\t%d:\t %s", s.Err, s.StartLine, s.Seq)
+	}
+	if len(s.Qual) == 0 {
+		return fmt.Sprintf(">%s\n%s", s.Id, s.Seq)
+	}
 	qs := make([]string, len(s.Qual))
 	for i, qq := range s.Qual {
 		qs[i] = string(qq + s.QBase)
@@ -91,10 +109,10 @@ func (s *simpleSeq) String() string {
 }
 
 // IUPACBases is a map of valid IUPAC bases.
-var IUPACBases map[rune]bool
+var IUPACBases map[byte]bool
 
 func init() {
-	IUPACBases = map[rune]bool{
+	IUPACBases = map[byte]bool{
 		'A': true,
 		'C': true,
 		'G': true,
@@ -111,18 +129,39 @@ func init() {
 		'V': true,
 		'N': true,
 		'U': true,
+		'a': true,
+		'c': true,
+		'g': true,
+		't': true,
+		'r': true,
+		'y': true,
+		's': true,
+		'w': true,
+		'k': true,
+		'm': true,
+		'b': true,
+		'd': true,
+		'h': true,
+		'v': true,
+		'n': true,
+		'u': true,
 	}
 
 }
 
-// validateSeqString check for illegal bases.
-func validateSeqString(dna string) error {
+// validateSeqBytes check for illegal bases.
+func validateSeqBytes(dna []byte) error {
 	for i, base := range dna {
 		if !IUPACBases[base] {
 			return errors.New(fmt.Sprintf("Illegal base '%s' at position %d", string(base), i))
 		}
 	}
 	return nil
+}
+
+// validateSeqString check for illegal bases.
+func validateSeqString(dna string) error {
+	return validateSeqBytes([]byte(dna))
 }
 
 // validateQuals checks for negative quality values.
@@ -149,62 +188,318 @@ func ValidateSeq(seq *simpleSeq) error {
 	return nil
 }
 
-// NewRawSeqStream initializes a new channel for reading fastq records in a robust way.
-func NewRawSeqStream(inFastq string, chanSize int, qBase int) chan *simpleSeq {
-	seqChan := make(chan *simpleSeq, chanSize)
+// NewRawSeqStream initializes a new channel for reading fastq records from a file in a robust way.
+func NewRawSeqStreamFromFile(inFastq string, chanSize int, qBase int, format string) chan *simpleSeq {
 	r, err := xopen.Ropen(inFastq)
 	checkError(err)
-
+	buffSize := 100 * 1024
+	ctrlChan := make(chan SeqStreamCtrl, 0)
 	go func() {
-		var token string
-		var err error
-		var inRecord int
-		var lineNr int
-		var s *simpleSeq
-
 		for {
-
-			// Read in the next line:
-			token, err = r.ReadString('\n')
-			token = strings.TrimRight(token, "\n")
-
-			// Reached end of file,
-			// send last record:
-			if err == io.EOF {
-				seqChan <- s
-				close(seqChan)
-				break
-			} else if err != nil {
-				panic(err)
-			} else {
-
-				if token[0] == '@' && inRecord == 0 {
-					// Found a new header line:
-					if s != nil {
-						seqChan <- s
+			select {
+			case i := <-ctrlChan:
+				if i == StreamEOF {
+					ctrlChan <- StreamQuit
+					i = <-ctrlChan
+					if i == StreamExited {
+						return
 					}
-					inRecord++
-					s = new(simpleSeq)
-					s.Id = string(token[1:])
+				}
+			case ctrlChan <- StreamTry:
+			default:
+			}
+		}
+	}()
+	switch format {
+	case "fastq":
+		return NewRawFastqStream(bufio.NewReaderSize(r, buffSize), chanSize, qBase, inFastq, ctrlChan)
+	case "fasta":
+		return NewRawFastaStream(bufio.NewReaderSize(r, buffSize), chanSize, inFastq, ctrlChan)
+	}
+	return nil
+}
 
-				} else if inRecord == 1 {
-					// Sequence line:
-					s.Seq = token
-					inRecord++
-				} else if inRecord == 2 {
-					// Skip separator line:
-					inRecord++
-				} else if inRecord == 3 {
-					// Quality line:
-					s.Qual = parseQuals(token, qBase)
-					s.QBase = qBase
-					inRecord = 0 // Expect header next time.
+type FqlState struct {
+	Header  bool
+	Seq     bool
+	Plus    bool
+	Qual    bool
+	Partial bool
+	Invalid bool
+}
+
+type FqLine struct {
+	Line     string
+	FqlState FqlState
+}
+type FqLines []FqLine
+
+func guessFqlState(line []byte) FqlState {
+	state := FqlState{}
+	switch line[0] {
+	case '@':
+		state.Header = true
+		state.Qual = true
+	case '+':
+		if len(line) == 1 {
+			state.Plus = true
+		} else {
+			state.Qual = true
+		}
+	default:
+		state.Seq = true
+		state.Qual = true
+	}
+	return state
+}
+
+func guessFasState(line []byte) FqlState {
+	state := FqlState{}
+	switch line[0] {
+	case '>':
+		if len(line) > 1 {
+			state.Header = true
+		} else {
+			state.Invalid = true
+		}
+	default:
+		if validateSeqBytes(line) == nil {
+			state.Seq = true
+		} else {
+			state.Invalid = true
+		}
+	}
+	return state
+}
+
+func FqLinesToSimpleSeq(lines FqLines, qBase int) (*simpleSeq, error) {
+	if len(lines) != 4 {
+		return nil, errors.New("Line buffer must have 4 lines!")
+	}
+	lh, ls, lp, lq := &lines[0], &lines[1], &lines[2], &lines[3]
+	if lh.FqlState.Header && ls.FqlState.Seq && lp.FqlState.Plus && lq.FqlState.Qual {
+		seq := &simpleSeq{lh.Line[1:], ls.Line, parseQuals(lq.Line, qBase), qBase, nil, -1}
+		seq.Err = ValidateSeq(seq)
+		if seq.Err != nil {
+			return nil, seq.Err
+		}
+		return seq, seq.Err
+	} else {
+		return nil, errors.New("Invalid line states!")
+	}
+	return nil, nil
+}
+
+func FasLinesToSimpleSeq(lines FqLines) (*simpleSeq, error) {
+	if len(lines) < 2 {
+		return nil, errors.New("Line buffer must have 4 lines!")
+	}
+	if !lines[0].FqlState.Header {
+		return nil, errors.New("Missing header line!")
+	}
+	s := &simpleSeq{Id: lines[0].Line[1:]}
+	for i := 1; i < len(lines); i++ {
+		if lines[i].FqlState.Invalid && !lines[i].FqlState.Seq {
+			return nil, errors.New("Invalid line structure!")
+		}
+		s.Seq += lines[i].Line
+	}
+	return s, nil
+}
+
+func streamFastq(r *bufio.Reader, sbuff FqLines, out chan *simpleSeq, ctrlChan chan SeqStreamCtrl, lineCounter *int, qBase int) (FqLines, error) {
+	var line []byte
+	var lastLine *FqLine
+	if len(sbuff) > 0 {
+		lastLine = &sbuff[len(sbuff)-1]
+	}
+	var err error
+	for {
+		line, err = r.ReadBytes('\n')
+		switch err {
+		case nil:
+			line = bytes.Trim(line, "\n\t ")
+			if len(line) == 0 {
+				*lineCounter++
+				continue
+			}
+			if lastLine != nil && lastLine.FqlState.Partial {
+				*lineCounter++
+				lastLine.Line += string(line)
+				lastLine.FqlState = guessFqlState([]byte(lastLine.Line))
+			} else {
+				*lineCounter++
+				lineStr := string(line)
+				sbuff = append(sbuff, FqLine{lineStr, guessFqlState(line)})
+				lastLine = &sbuff[len(sbuff)-1]
+			}
+			if len(sbuff) == 4 && !lastLine.FqlState.Partial {
+				seq, err := FqLinesToSimpleSeq(sbuff, qBase)
+				if err == nil {
+					seq.StartLine = *lineCounter - 4
+					out <- seq
+					sbuff = sbuff[:0]
 				} else {
-					fmt.Fprintf(os.Stderr, "Skipping input line %d!\n", lineNr)
+					h := -1
+					for i := 1; i < len(sbuff); i++ {
+						if sbuff[i].FqlState.Header {
+							h = i
+							break
+						}
+					}
+					if h < 0 {
+						h = len(sbuff)
+					}
+					for j := 0; j < h; j++ {
+						serr := &simpleSeq{StartLine: *lineCounter - j, Err: errors.New("Discarded line"), Seq: sbuff[j].Line}
+						out <- serr
+					}
+					sbuff = sbuff[h:]
+
+				}
+			} //sbuff == 4
+		case io.EOF:
+			line = bytes.TrimRight(line, "\n")
+			if len(line) > 0 {
+				sbuff = append(sbuff, FqLine{string(line), FqlState{Partial: true}})
+			}
+			ctrlChan <- StreamEOF
+			return sbuff, nil
+		default:
+			return sbuff, err
+		}
+
+	}
+	return sbuff, nil
+}
+
+func streamFasta(r *bufio.Reader, sbuff FqLines, out chan *simpleSeq, ctrlChan chan SeqStreamCtrl, lineCounter *int, final bool) (FqLines, error) {
+	var line []byte
+	var lastLine *FqLine
+	if len(sbuff) > 0 {
+		lastLine = &sbuff[len(sbuff)-1]
+	}
+	var err error
+	for {
+		line, err = r.ReadBytes('\n')
+		switch err {
+		case nil:
+			line = bytes.TrimRight(line, "\n\t ")
+			if len(line) == 0 {
+				*lineCounter++
+				continue
+			}
+			if lastLine != nil && lastLine.FqlState.Partial {
+				*lineCounter++
+				lastLine.Line += string(line)
+				lastLine.FqlState = guessFasState([]byte(lastLine.Line))
+			} else {
+				*lineCounter++
+				lineStr := string(line)
+				sbuff = append(sbuff, FqLine{lineStr, guessFasState(line)})
+				lastLine = &sbuff[len(sbuff)-1]
+			}
+			if len(sbuff) > 2 && !lastLine.FqlState.Partial {
+				if sbuff[0].FqlState.Header && (lastLine.FqlState.Header || final) {
+					seq, err := FasLinesToSimpleSeq(sbuff[:len(sbuff)-1])
+					if err == nil {
+						seq.StartLine = *lineCounter - 4
+						out <- seq
+						sbuff = sbuff[len(sbuff)-1:]
+					} else {
+						for j := 0; j < len(sbuff); j++ {
+							serr := &simpleSeq{StartLine: *lineCounter - j, Err: errors.New("Discarded line"), Seq: sbuff[j].Line}
+							out <- serr
+						}
+					}
+					sbuff = sbuff[len(sbuff)-1:]
 				}
 			}
+		case io.EOF:
+			line = bytes.TrimRight(line, "\n")
+			if len(line) > 0 {
+				sbuff = append(sbuff, FqLine{string(line), FqlState{Partial: true}})
+			}
+			ctrlChan <- StreamEOF
+			return sbuff, nil
+		default:
+			return sbuff, err
+		}
 
-			lineNr++
+	}
+	return sbuff, nil
+}
+
+// NewRawSeqStream initializes a new channel for reading fastq records in a robust way.
+func NewRawFastqStream(inReader *bufio.Reader, chanSize int, qBase int, id string, ctrlChan chan SeqStreamCtrl) chan *simpleSeq {
+	seqChan := make(chan *simpleSeq, chanSize)
+	lineCounter := 0
+
+	go func() {
+		sbuff := make(FqLines, 0, 100)
+		var err error
+		_ = err
+
+		for {
+			select {
+			case cmd := <-ctrlChan:
+				if cmd == StreamTry {
+					sbuff, err = streamFastq(inReader, sbuff, seqChan, ctrlChan, &lineCounter, qBase)
+					if err != nil {
+						panic(err)
+					}
+
+				} else if cmd == StreamQuit {
+					sbuff, err = streamFastq(inReader, sbuff, seqChan, ctrlChan, &lineCounter, qBase)
+					for i, l := range sbuff {
+						serr := &simpleSeq{Err: errors.New("Discarded line"), StartLine: lineCounter - i, Seq: l.Line}
+						seqChan <- serr
+					}
+					close(seqChan)
+					ctrlChan <- StreamExited
+					return
+				} else {
+					panic("Invalid stream control command!")
+				}
+			}
+		}
+	}()
+
+	return seqChan
+}
+
+// NewRawSeqStream initializes a new channel for reading fastq records in a robust way.
+func NewRawFastaStream(inReader *bufio.Reader, chanSize int, id string, ctrlChan chan SeqStreamCtrl) chan *simpleSeq {
+	seqChan := make(chan *simpleSeq, chanSize)
+	lineCounter := 0
+
+	go func() {
+		sbuff := make(FqLines, 0, 100)
+		var err error
+		_ = err
+
+		for {
+			select {
+			case cmd := <-ctrlChan:
+				if cmd == StreamTry {
+					sbuff, err = streamFasta(inReader, sbuff, seqChan, ctrlChan, &lineCounter, false)
+					if err != nil {
+						panic(err)
+					}
+
+				} else if cmd == StreamQuit {
+					sbuff, err = streamFasta(inReader, sbuff, seqChan, ctrlChan, &lineCounter, true)
+					for i, l := range sbuff {
+						serr := &simpleSeq{Err: errors.New("Discarded line"), StartLine: lineCounter - i, Seq: l.Line}
+						seqChan <- serr
+					}
+					close(seqChan)
+					ctrlChan <- StreamExited
+					return
+				} else {
+					panic("Invalid stream control command!")
+				}
+			}
 		}
 	}()
 
